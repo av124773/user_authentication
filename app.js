e71e47656e31e6917ee7d150a1c33fddfcdee29a
av124773 <av124773@gmail.com>
2023-03-03 17:23:16 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const PORT = 3001
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoose error!')
+})
+db.once('open', () => {
+  console.log('mongoose connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('express test')
